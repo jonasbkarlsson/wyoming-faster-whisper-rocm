5d4a36808a1c483c504968a35ef995a215a1e868
Jonas Karlsson <jonas.b.karlsson@gmail.com>
2025-08-29 23:15:04 +0200

    Update run parameters

--- run.sh
+++ run.sh
@@ -15,27 +15,13 @@
 #   ctranslate2-rocm
 
 docker run -d \
-  --network=host  \
   --device /dev/kfd \
+  --device /dev/dri \
   --group-add=video  \
   --ipc=host \
   --cap-add=SYS_PTRACE \
   --security-opt seccomp=unconfined \
   --shm-size 8G \
+  -p 10300:10300 \
   -v ./data:/data \
-  -v $HOME/dockerx:/dockerx \
-  -w /dockerx \
   wyoming-faster-whisper-rocm
-
-#  --privileged \
-#  --device /dev/kfd \
-#  --device /dev/dri \
-#   --group-add=render  \
-
-
-#  --privileged \
-
-#docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add=video --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --shm-size 8G -v ./data:/data -v $HOME/dockerx:/dockerx -w /dockerx   wyoming-faster-whisper-rocm
-
-
-#  ctranslate2-rocm
--- start_faster_whisper.sh
+++ start_faster_whisper.sh
@@ -8,13 +8,13 @@ docker rm faster-whisper || true
 
 docker run -d \
   --name faster-whisper \
+  --device /dev/kfd \
+  --device /dev/dri \
+  --group-add=video  \
+  --ipc=host \
   --cap-add=SYS_PTRACE \
   --security-opt seccomp=unconfined \
-  --ipc=host \
   --shm-size 8G \
-  --device=/dev/dxg \
-  -v /usr/lib/wsl/lib/libdxcore.so:/usr/lib/libdxcore.so \
-  -v /opt/rocm/lib/libhsa-runtime64.so.1:/opt/rocm/lib/libhsa-runtime64.so.1 \
+  -p 10300:10300 \
   -v ./data:/data \
-  -p "10300:10300" \
   wyoming-faster-whisper-rocm
