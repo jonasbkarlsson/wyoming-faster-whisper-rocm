7d19d31c46739b48ee52f8eaedd83e5e349df82a
Jonas Karlsson <jonas.b.karlsson@gmail.com>
2025-08-30 15:25:38 +0200

    Added  args in docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,22 @@
 FROM rocm/dev-ubuntu-22.04:6.4.2-complete
 # Includes Python 3.10
 
+ARG GFX
+ARG WHISPER_MODEL
+ENV WHISPER_MODEL=${WHISPER_MODEL}
+
 RUN pip install -U pip && pip install wyoming==1.7.2 faster-whisper==1.2.0 tokenizers==0.21.*
 RUN apt-get update && apt-get -y install nano ffmpeg libomp-dev git cmake
 
 COPY src /src
 WORKDIR /src
+RUN chmod +x *.sh
 
 RUN git clone https://github.com/arlo-phoenix/CTranslate2-rocm.git --recurse-submodules
 
-ENV PYTORCH_ROCM_ARCH=gfx1101
+ENV PYTORCH_ROCM_ARCH=${GFX}
 # gfx908;gfx90a;gfx1030;gfx1100;gfx1101;gfx942;gfx1200;gfx1201
 RUN ./build.sh
 
-ENTRYPOINT ["/src/run.sh"]
+ENTRYPOINT ["sh", "-c", "/src/run.sh --model ${WHISPER_MODEL}"]
 # ENTRYPOINT ["tail", "-f", "/dev/null"]
--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-#conda init bash
-#source ~/.bashrc
-#conda activate py_3.10
+# conda init bash
+# source ~/.bashrc
+# conda activate py_3.10
 
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
-#export LD_PRELOAD=$LD_PRELOAD:/lib/x86_64-linux-gnu/libomp.so.5
-#export HSA_OVERRIDE_GFX_VERSION=9.0.0
-# exec python3 -m wyoming_whisper --device cuda --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data "$@"
-# exec python3 -m wyoming_whisper --device cuda --model distil-small.en --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data --debug "$@"
-exec python3 -m wyoming_whisper --device cuda --model large-v3 --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data --debug "$@"
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
+# export HSA_OVERRIDE_GFX_VERSION=9.0.0
+exec python3 -m wyoming_whisper --device cuda --uri 'tcp://0.0.0.0:10300' --data-dir /data --download-dir /data "$@"
